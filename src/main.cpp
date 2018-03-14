49c6184ec15d06df6efa9eddf3d90ceee7b00be1
3jackattack3 <3jackattack3@gmail.com>
2018-03-14 11:11:29 -0400

    fixed! was using the wrong pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, 2, NEO_RGBW + NEO_KHZ800);
 
 void setup() {
-    // put your setup code here, to run once:
+  // pinMode(2, OUTPUT);
+
+  clock_prescale_set(clock_div_1);
+
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+
+  // strip.setPixelColor(0, strip.Color(0, 250, 0));
+  // strip.show();
+  // delay(500);
+  //
+  // strip.setPixelColor(0, strip.Color(0, 0, 0));
+  // strip.show();
+  // delay(500);
+
+
+  for(int i = 0; i<=255; i++){
+    strip.setPixelColor(0, strip.Color(0, i, 255-i));
+    strip.show();
+    delay(10);
+  }
+  for(int i = 255; i>=0; i--){
+    strip.setPixelColor(0, strip.Color(0, i, 255-i));
+    strip.show();
+    delay(10);
+  }
 }
